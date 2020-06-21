f1d0642e3ecd38ef5fbf2407f9cef34f7fb38e5a
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-21 12:23:27 -0400

    Web scraping for Hacker News.

--- scrape.py
+++ scrape.py
@@ -1,8 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
-response = requests.get('https://news.ycombinator.com/news')
+imporant_stories = []
 
-soup = BeautifulSoup(response.text, 'html.parser')
 
-print(soup)
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    for i, link in enumerate(links):
+        title = link.getText()
+        href = link.get('href', None)
+        vote = subtext[i].select('.score')
+        if vote:
+            points = int(vote[0].getText().replace(' points', ''))
+
+            if points > 100:
+                imporant_stories.append({
+                    'title': title,
+                    'link': href,
+                    'votes': points
+                })
+
+
+def scape_hn(pages):
+    for page_number in range(pages):
+        page = requests.get(
+            f'https://news.ycombinator.com/news?p={page_number + 1}')
+        soup = BeautifulSoup(page.text, 'html.parser')
+        links = soup.select('.storylink')
+        subtext = soup.select('.subtext')
+
+        create_custom_hn(links, subtext)
+
+    return sort_stories_by_votes(imporant_stories)
+
+
+pprint.pprint(scape_hn(5))
